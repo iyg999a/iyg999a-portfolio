882295a1c160893b2015d7a171c84dfb06d8d698
iyg999a <abhishekyadugani@gmail.com>
2026-07-01 00:00:33 +0530

    v1.0.1

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json())
 connectDB()
 app.use("/api/contact", contactRoutes);
-app.use("/", ()=>{
+app.use("/", (req, res)=>{
     res.send("Running")
 })
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("server started")
 })
